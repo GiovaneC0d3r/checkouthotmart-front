d6a07ab184272a8c2f40ce66e5e492bb560c4bea
Giovane <98927748+GiovaneC0d3r@users.noreply.github.com>
2025-11-20 12:20:03 -0400

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import PixView from '@/views/PixView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'pix',
-      component: PixView
+      component: () => import('@/views/PixView.vue'),
     },
     {
       path: '/form',
